6edb31eba84bfeb40e151dff7941b587d0432a40
jonathangoulding <jonathangoulding21@gmail.com>
2021-08-11 22:04:10 +0100

    feat: add dot env

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/api', router);
