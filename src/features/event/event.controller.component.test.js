d1b8631ed2b17a44c825ad087b2f30b232f62a70
jonathangoulding <jonathangoulding21@gmail.com>
2021-08-11 15:07:51 +0100

    test: get event by id

--- src/features/event/event.controller.component.test.js
+++ src/features/event/event.controller.component.test.js
@@ -5,7 +5,7 @@ describe('event controller', () => {
       const eventData = require('../../database/MOCK_DATA.json');
       const req = {};
       const res = {
-        json: (a) => a,
+        json: (obj) => obj,
       };
       // Act
       const { get } = require('./event.controller');
@@ -13,6 +13,62 @@ describe('event controller', () => {
       // Assert
       it('then should return all the events', () => {
         expect(response).toEqual(eventData);
+      });
+    });
+  });
+
+  describe('getById', () => {
+    describe('when getting an event by id', () => {
+      // Arrange
+      const id = 1;
+      let response;
+      beforeEach(() => {
+        const req = {
+          params: {
+            id,
+          },
+        };
+        const res = {
+          json: (obj) => obj,
+        };
+        // Act
+        const { getById } = require('./event.controller');
+        response = getById(req, res);
+      });
+
+      // Assert
+      it('then should return all the events', () => {
+        expect(response).toEqual({
+          id: 1,
+          location: '43 Ridge Oak Drive',
+          name: "Man Who Planted Trees, The (Homme qui plantait des arbres, L')",
+          type: 'Animation|Drama',
+        });
+      });
+    });
+    describe('when getting an event by an id that does not exist', () => {
+      // Arrange
+      const id = -1;
+      let response;
+      beforeEach(() => {
+        const req = {
+          params: {
+            id,
+          },
+        };
+        const res = {
+          status: (status) => status,
+          json: (obj) => obj,
+        };
+        // Act
+        const { getById } = require('./event.controller');
+        response = getById(req, res);
+      });
+      // Assert
+      it('then should return all the events', () => {
+        expect(response).toEqual({
+          message: 'Not Found',
+        });
       });
     });
   });
--- src/features/event/event.controller.unit.test.js
+++ src/features/event/event.controller.unit.test.js
@@ -3,19 +3,81 @@ describe('event controller', () => {
     describe('when getting all events', () => {
       // Arrange
       const mockEventData = 'mock-data';
-      jest.mock('../../database', () => ({
-        getAllEvents: jest.fn().mockReturnValue(mockEventData),
-      }));
-      const req = {};
-      const res = {
-        json: (a) => a,
-      };
-      // Act
-      const { get } = require('./event.controller');
-      const response = get(req, res);
+      let response;
+      beforeEach(() => {
+        jest.mock('../../database', () => ({
+          getAllEvents: jest.fn().mockReturnValue(mockEventData),
+        }));
+        const req = {};
+        const res = {
+          json: (a) => a,
+        };
+        // Act
+        const { get } = require('./event.controller');
+        response = get(req, res);
+      });
+
       // Assert
       it('then should return all the events', () => {
         expect(response).toEqual(mockEventData);
+      });
+    });
+  });
+  describe('getById', () => {
+    describe('when getting an event by id', () => {
+      // Arrange
+      const id = 1;
+      let response;
+      const mockEventData = [{ id, name: 'fake event name' }];
+      beforeEach(() => {
+        jest.mock('../../database', () => ({
+          getEventById: jest.fn().mockReturnValue(mockEventData),
+        }));
+        const req = {
+          params: {
+            id,
+          },
+        };
+        const res = {
+          json: (obj) => obj,
+        };
+        // Act
+        const { getById } = require('./event.controller');
+        response = getById(req, res);
+      });
+
+      // Assert
+      it('then should return the matching event', () => {
+        expect(response).toEqual(mockEventData);
+      });
+    });
+    describe('when getting an event by an id that does not exist', () => {
+      // Arrange
+      const id = 1;
+      let response;
+      let mockEventData;
+      beforeEach(() => {
+        jest.mock('../../database', () => ({
+          getEventById: jest.fn().mockReturnValue(mockEventData),
+        }));
+        const req = {
+          params: {
+            id,
+          },
+        };
+        const res = {
+          status: (status) => status,
+          json: (obj) => obj,
+        };
+        // Act
+        const { getById } = require('./event.controller');
+        response = getById(req, res);
+      });
+      // Assert
+      it('then should return not found', () => {
+        expect(response).toEqual({
+          message: 'Not Found',
+        });
       });
     });
   });
